de6bedd447abe45bb1f33c5e7406b4dca04d777f
Laurence Sonnenberg <laurencesonnenberg01@gmail.com>
2022-01-07 13:25:03 +0200

    update gradle

--- library-test/build.gradle.kts
+++ library-test/build.gradle.kts
@@ -45,7 +45,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "io.robothouse"
             artifactId = "gradle-library-test"
-            version = "0.1.3"
+            version = "0.1.4"
 
             from(components["java"])
         }
